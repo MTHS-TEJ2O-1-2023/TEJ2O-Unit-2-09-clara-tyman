13239de18667f78ada0870aa8d890c0795da3b17
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-10-10 13:27:45 -0400

    rockpaperscissors start ts version

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara
+ * Created on: Oct 2023
+ * This program plays rock paper scissors on the micro bit
 */
 
-basic.showString('Hello, World!')
+let rockpaperscissors: number = -1
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function() {
+  rockpaperscissors = randint(0, 1)
+  basic.clearScreen()
+
+  // If random number is 0
+  if (rockpaperscissors == 0) {
+basic.showLeds(`
+. . . . .
+. # # # .
+. # # # .
+. # # # .
+. . . . .
+`)
+}
+
+    // If random number is 1
+    if (rockpaperscissors == 1) {
+basic.showLeds(`
+# # # # #
+# . . . #
+# . . . #
+# . . . #
+# # # # #
+`)
+    }
+
+    // If random number is 2
+    if (rockpaperscissors == 2) {
+basic.showIcon(IconNames.Scissors)
+    }
+
+  // pause and show you are ready again
+  basic.pause(1000)
+  basic.showIcon(IconNames.Happy)
+})
